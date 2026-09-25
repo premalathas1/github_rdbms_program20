2fba6da42117f220b33479723d8be3fa0b518df7
premalathas1 <premalathas@rvsgroup.com>
2026-09-25 15:28:49 +0530

    Remove employee insert trigger from answers.sql

--- answers.sql
+++ answers.sql
@@ -1,11 +1,1 @@
-SET SERVEROUTPUT ON;
 
-CREATE OR REPLACE TRIGGER employee_insert_trigger
-AFTER INSERT ON Employee
-FOR EACH ROW
-BEGIN
-DBMS_OUTPUT.PUT_LINE(
-'New employee record inserted successfully.'
-);
-END;
-/
